308527741484170edc7ca3d2d7cb92fb67b29192
Harshal Shah <harshal@infracloud.io>
2016-11-25 13:09:04 +0530

    Fixing windows CRLF issue

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
+if [ $(basename $PWD) != "scripts" ]; then
+   echo "ERROR: Run this script from scripts directory. Exiting"
+   exit 1
+fi
+
+#
+# Adding tr to fix line ending issue of windows 
+#
+for f in install*sh
+do
+tr -d '\015' $f > $f
+done
+
 cd ..;
+
 step=$1;
 if [ "$step" = "1" ]|| [ "$step" = "all" ]
  then
